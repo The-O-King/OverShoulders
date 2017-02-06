b84c98e015fb4c1932c6db565b247dd45dd39b4b
Atthar Mirza <atthar.mirza@gmail.com>
2017-02-05 23:43:38 -0500

    Began Level Design
    I set up a TEMP folder for a different Unreal project, since I'm running
    a newer version and can't revert back to 4.14.

---
+++ TEMP_LevelDesign/Intermediate/Build/Win64/UE4Editor/Development/OverShoulders/OverShoulders.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_OVERSHOULDERS_H__
+#define __AUTO_OVERSHOULDERS_H__
+//Last Write: 2/6/2017 3:53:21 AM +00:00
+#include "E:\ARefugeesShoulders\Repository\OverShoulders\Source\OverShoulders\OverShoulders.h"
+#endif//__AUTO_OVERSHOULDERS_H__
