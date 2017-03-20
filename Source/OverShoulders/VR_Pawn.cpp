f6bee0b43b24eb3f85d6be1e8b81625bb6c97511
The-O-King <techmanozmar@gmail.com>
2017-03-20 00:10:38 -0400

    VR_Pawn Interaction Fix
    Resolved issue with incorrect implementation of delegate method
    assignment to get overlap events working

--- Source/OverShoulders/VR_Pawn.cpp
+++ Source/OverShoulders/VR_Pawn.cpp
@@ -38,8 +38,8 @@ AVR_Pawn::AVR_Pawn()
 	Left->Hand = EControllerHand::Left;
 	MeshL = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshL"));
 	MeshL->SetupAttachment(Left);
-	//MeshL->OnComponentBeginOverlap.AddDynamic(MeshL, &UHandComponent::OnHandOverlapBegin);
-	//MeshL->OnComponentEndOverlap.AddDynamic(MeshL, &UHandComponent::OnHandOverlapEnd);
+	MeshL->OnComponentBeginOverlap.AddDynamic(Left, &UHandComponent::OnHandOverlapBegin);
+	MeshL->OnComponentEndOverlap.AddDynamic(Left, &UHandComponent::OnHandOverlapEnd);
 
 	Right = CreateDefaultSubobject<UHandComponent>(TEXT("Right"));
 	Right->SetupAttachment(Container);
@@ -47,8 +47,8 @@ AVR_Pawn::AVR_Pawn()
 	Right->Hand = EControllerHand::Right;
 	MeshR = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshR"));
 	MeshR->SetupAttachment(Right);
-	//MeshR->OnComponentBeginOverlap.AddDynamic(MeshL, &UHandComponent::OnHandOverlapBegin);
-	//MeshR->OnComponentEndOverlap.AddDynamic(MeshL, &UHandComponent::OnHandOverlapEnd);
+	MeshR->OnComponentBeginOverlap.AddDynamic(Right, &UHandComponent::OnHandOverlapBegin);
+	MeshR->OnComponentEndOverlap.AddDynamic(Right, &UHandComponent::OnHandOverlapEnd);
 
 	// Create SteamVR Chaperone
 	SteamVR = CreateDefaultSubobject<USteamVRChaperoneComponent>(TEXT("SteamVRChaperoneComponent"));
